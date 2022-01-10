2aec2b646196d49b567c150629a7b9f1902538a6
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-01-10 17:07:05 -0300

    fix create table

--- flaskapp/app/__init__.py
+++ flaskapp/app/__init__.py
@@ -3,7 +3,9 @@ from flask import render_template
 from app.routes import services_routes
 from app.services.db import app,db
 
+# !Temporário
 db.create_all()
+# app = create_app()
 
 @app.route('/')
 def index():    
