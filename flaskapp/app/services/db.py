3a08460705f3ff360049bd23542fc6fad51ca564
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-01-11 11:35:52 -0300

    add initial frontend

--- flaskapp/app/services/db.py
+++ flaskapp/app/services/db.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__,template_folder='../templates')
+app = Flask(__name__,template_folder='../templates',static_folder='../static')
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlite3.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["FLASK_DB_SEEDS_PATH"] = "app/migrations/seeds.py"
