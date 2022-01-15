60d412b8e1f104621f03db9ac8aa5c8328ad430a
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-01-15 14:50:26 -0300

    add try catch in serviceController

--- flaskapp/app/Controllers/serviceController.py
+++ flaskapp/app/Controllers/serviceController.py
@@ -3,11 +3,14 @@ from flask import jsonify,Response
 
 class ServiceController:
     def create(self,name):
-        if not Service.find_by_name(name.upper()) is None:
-            return Response('{"error":"service already exist"}', status=400, mimetype='application/json')
-        newService = Service(**{"name":name.upper()}).save()
-        return jsonify(
-            id=newService.id,
-            createdAt=newService.created_at,
-            name=newService.name,
-        )
+        try:
+            if not Service.find_by_name(name.upper()) is None:
+                return Response('{"error":"service already exist"}', status=400, mimetype='application/json')
+            newService = Service(**{"name":name.upper()}).save()
+            return jsonify(
+                id=newService.id,
+                createdAt=newService.created_at,
+                name=newService.name,
+            )
+        except: 
+            return Response('{"error":"server error"}', status=500, mimetype='application/json')
