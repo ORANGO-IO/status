4c359eedce868f5710a7e4c5e27561f0e0a76422
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-01-31 13:29:20 -0300

    add new seeds to teste

--- flaskapp/app/migrations/seeds.py
+++ flaskapp/app/migrations/seeds.py
@@ -3,10 +3,14 @@
 # Example:
 from flask import current_app
 from app.Models.JopRecordStatus import JopRecordStatus
+from app.Models.Service_group import Service_group
+from app.Models.Service import Service
+from app.Models.Job import Job
+from app.Models.JobRecord import JobRecord
 # from app.services.db import db
 app_config = current_app.config
 
-print("oioi")
+print("run seeds")
 initialStatus = [
     {
         "value":"online",
@@ -18,9 +22,35 @@ initialStatus = [
     }
 ]
 
+service_group = {
+    "name":"Lithocenter"
+}
+
+service = {
+    "name":"teste",
+    "service_group_id":1
+}
+
+job = {
+	"order":1,
+	"url":"http://lithocenterhospitaldia.com/",
+	"action":"XPATH",
+	"actionValue":"//div[@class='phone']/a[@class='number']",
+	"serviceId":1
+}
+
+jobRecord={
+    "job_id":1,
+    "status_id":1,
+    "time_spent_in_sec":321312,
+}
+
 for x in initialStatus:
     if JopRecordStatus.find_by_name(x['value']) is None:
         JopRecordStatus(**x).save()
-
-# db.session.add(status)
-# db.session.commit()
+Service_group(**service_group).save()
+Service(**service).save()
+Job(**job).save()
+JobRecord(**jobRecord).save()
+JobRecord(**jobRecord).save()
+print("finished seeds")
