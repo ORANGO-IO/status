898f1e1e0bb14370449f8e97b075ab8897bf46b1
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-02-07 16:27:50 -0300

    add data of backend in frontend

--- flaskapp/app/__init__.py
+++ flaskapp/app/__init__.py
@@ -2,16 +2,19 @@ from flask import render_template
 
 from app.routes import services_routes
 from app.services.db import app, db
+from app.Controllers.job_record import Job_record_controller
+
+job_record_controller = Job_record_controller()
 
 @app.route('/')
 def index():
     ''' Aqui eu devo capturar os status e imagens '''
-    return render_template('status.html')
+    return render_template('status.html',json = job_record_controller.all())
 
 
 @app.route('/details')
 def details():
-    return render_template('details.html')
+    return render_template('details.html',json = job_record_controller.all())
 
 
 app.register_blueprint(services_routes, url_prefix='/service')
