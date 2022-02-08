71e6ff2f6d57294b30e10e42caac2ac9d48a4f63
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-02-08 17:49:45 -0300

    add jobs,services and service_groups in seed

--- flaskapp/app/migrations/seeds.py
+++ flaskapp/app/migrations/seeds.py
@@ -22,35 +22,227 @@ initialStatus = [
     }
 ]
 
-service_group = {
-    "name": "lithocenter"
-}
+service_group = [
+    {
+     "name": "lithocenter"
+    },
+    {
+        "name": "postbaker"
+    },
+    {
+        "name": "orango"
+    },
+    {
+        "name":"Blog Orango"
+    },
+    {
+        "name":"itmpr"
+    },
+    {
+        "name":"Filipe lopes"
+    },
+    {
+        "name":"Calc"
+    },
+    {
+        "name":"clicklav"
+    },
+    {
+        "name":"teleatendimento"
+    }
+]
 
-service = {
-    "name": "teste",
-    "service_group_id": 1
-}
+service = [
+    {
+    "name": "frontend",
+    "service_group_id": 3
+},
+ {
+    "name": "frontend",
+    "service_group_id": 4
+},{
+    "name": "backend",
+    "service_group_id": 4
+},{
+    "name": "database",
+    "service_group_id": 4
+},
+{
+    "name":"Painel adiministrativo",
+    "service_group_id":4,
+},
+{
+    "name":"frontend",
+    "service_group_id":2,
+},
+{
+    "name":"backend",
+    "service_group_id":2,
+},
+{
+    "name":"database",
+    "service_group_id":2
+},
+{
+    "name":"frontend",
+    "service_group_id":5,
+},
+{
+    "name":"frontend beta",
+    "service_group_id":5,
+},
+{
+    "name":"frontend beta",
+    "service_group_id":9
+},
+{
+    "name":"backend beta",
+    "service_group_id":9
+},
+{
+    "name":"banco de dados beta",
+    "service_group_id":9
+},
+{
+    "name":"frontend",
+    "service_group_id":9
+},
+{
+    "name":"backend",
+    "service_group_id":9
+},
+{
+    "name":"banco de dados",
+    "service_group_id":9
+},
+{
+    "name":"frontend",#17
+    "service_group_id":6
+},
+{
+    "name":"backend",
+    "service_group_id":6
+},
+{
+    "name":"banco de dados",
+    "service_group_id":6
+},
+{
+    "name":"frontend",
+    "service_group_id":7
+},
+{
+    "name":"frontend beta",
+    "service_group_id":8 #21
+},
+{
+    "name":"backend beta",
+    "service_group_id":8
+},
+{
+    "name":"banco de dados beta",
+    "service_group_id":8
+},
+{
+    "name":"frontend",
+    "service_group_id":8
+},
+{
+    "name":"backend",
+    "service_group_id":8
+},
+{
+    "name":"banco de dados",
+    "service_group_id":8
+},
 
-job = {
+]
+job = [{
     "order": 1,
-    "url": "http://lithocenterhospitaldia.com/",
+    "url": "https://orango.io/",
     "action": "XPATH",
-    "action_value": "//div[@class='phone']/a[@class='number']",
+    "action_value": "//div[@class='header']/img[@alt='logo']",
     "service_id": 1
-}
-
-jobRecord = {
-    "job_id": 1,
-    "status_id": 1,
-    "time_spent_in_sec": 321312
-}
+},
+{
+    "order": 1,
+    "url": "https://strapi.orango.io/",
+    "action": "XPATH",
+    "action_value": "//section[@class='wrapper']/h1/img[@alt='logo']",
+    "service_id": 5
+},
+{
+    "order": 1,
+    "url": "https://strapi.orango.io/admin/auth/login",
+    "action": "XPATH",
+    "action_value": "//section/img[@alt='strapi-logo']",
+    "service_id": 5
+},
+{
+    "order": 1,
+    "url": "https://beta.itmpr.com.br/adm/",
+    "action": "XPATH",
+    "action_value": "//div[@id='intro-logo']/img",
+    "service_id": 10,
+},
+{
+    "order": 1,
+    "url": "https://itmpr.com.br",
+    "action": "XPATH",
+    "action_value": "//header[@id='mainheader']/img[@id='mainlogo']",
+    "service_id": 9,
+},
+{
+    "order": 1,
+    "url": "https://teleatendimento.orango.io/login",
+    "action": "XPATH",
+    "action_value": "//header/img[@alt='lines']",
+    "service_id": 11,
+},
+{
+    "order": 1,
+    "url": "https://teleatendimento.orango.io/api/v1/",
+    "action": "status",
+    "action_value": "",
+    "service_id": 12,
+},
+{
+    "order": 1,
+    "url": "https://teleatendimento.orango.io/api/v1/alembic_version",
+    "action": "status",
+    "action_value": "alembic_version",
+    "service_id": 16,
+},
+{
+    "order": 1,
+    "url": "https://filipelopes.me/",
+    "action": "XPATH",
+    "action_value": "//header/nav/div/a@[href='#me']",
+    "service_id": 17,
+},
+{
+    "order": 1,
+    "url": "https://clicklav.com.br/",
+    "action": "XPATH",
+    "action_value": "//header/div/svg[@class='logo']",
+    "service_id": 21,
+},
+]
 
 for x in initialStatus:
     if JopRecordStatus.find_by_name(x['value']) is None:
         JopRecordStatus(**x).save()
-Service_group(**service_group).save()
-Service(**service).save()
-Job(**job).save()
-JobRecord(**jobRecord).save()
-JobRecord(**jobRecord).save()
+for x in service_group:
+    if Service_group.find_by_name(x['name']) is None:
+        Service_group(**x).save()
+for x in service:
+    if Service.find_by_name(x['name'],x['service_group_id']) is None:
+        Service(**x).save()
+for x in job:
+    Job(**x).save()
+# Service_group(**service_group).save()
+# Service(**service).save()
+# Job(**job).save()
+# JobRecord(**jobRecord).save()
+# JobRecord(**jobRecord).save()
 print("finished seeds")
