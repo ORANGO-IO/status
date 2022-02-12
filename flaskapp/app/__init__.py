d1ff0840d209ad8bcdc81796e8676b96b757239c
Filipe Lopes <filiperocklopes@gmail.com>
2022-02-12 13:52:56 -0300

    add cron job as example

--- flaskapp/app/__init__.py
+++ flaskapp/app/__init__.py
@@ -1,20 +1,30 @@
-from flask import render_template
+from flask import render_template, jsonify
 
 from app.routes import services_routes
 from app.services.db import app, db
 from app.Controllers.job_record import Job_record_controller
 
+
 job_record_controller = Job_record_controller()
+
 
 @app.route('/')
 def index():
     ''' Aqui eu devo capturar os status e imagens '''
-    return render_template('status.html',json = job_record_controller.all())
+    return render_template('status.html', json=job_record_controller.all())
 
 
-@app.route('/details')
+@app.route('/cron_job_test')
+def cron_job_test():
+    app.logger.info("Testing cron route")
+    response = jsonify(success=True)
+    response.status_code = 200
+    return response
+
+
+@ app.route('/details')
 def details():
-    return render_template('details.html',json = job_record_controller.all())
+    return render_template('details.html', json=job_record_controller.all())
 
 
 app.register_blueprint(services_routes, url_prefix='/service')
