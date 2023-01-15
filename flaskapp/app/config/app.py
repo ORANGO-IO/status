d807ac79fb12c7fcb62501c5eb0e4d4f37204f71
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2023-01-15 13:20:08 -0300

    fix: add env in app.py

--- flaskapp/app/config/app.py
+++ flaskapp/app/config/app.py
@@ -1,20 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_scss import Scss
+from dotenv import load_dotenv
+import os
 
-settings = {
-    "SQLALCHEMY_DATABASE_URI": "sqlite:///../services/sqlite3.db",
-    "SQLALCHEMY_TRACK_MODIFICATIONS": False,
-    "FLASK_DB_SEEDS_PATH": "app/migrations/seeds.py",
-    "SQLALCHEMY_ECHO":False,
-}
-
+load_dotenv()
 app = Flask(__name__,template_folder='../templates',static_folder='../static')
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../services/sqlite3.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DB_URL_CONFIG")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = False
 app.config["FLASK_DB_SEEDS_PATH"] = "app/migrations/seeds.py"
-app.config['TIMEZONE'] =  "America/Sao_Paulo"
+app.config['TIMEZONE'] =  os.getenv("TIMEZONE")
 
 db = SQLAlchemy(app)
 Scss(app,static_dir="app/static/css",asset_dir="app/static/sass")
@@ -22,7 +18,6 @@ Scss(app,static_dir="app/static/css",asset_dir="app/static/sass")
 def create_app():
     app = Flask(__name__)
 
-    app.config.update(settings)
 
     db.init_app(app)
 
